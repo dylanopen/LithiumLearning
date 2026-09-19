2e7f8495f0cb0b37b2418f5d4ddc3b9b04b8490e
dylancode <opensource@dylancode.dev>
2026-09-19 19:10:21 +0100

    Fix My Progress button to link to /stats (instead of /courses)

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,5 +1,5 @@
 <header class="sticky top-0 z-40 border-b border-slate-200">
     <a class="lithium-logo" href="/">Lithium Learning</a>
     <a class="nav-button discover-topics-button" href="/courses">Discover Topics</a>
-    <a class="nav-button my-progress-button" href="/courses">My Progress</a>
+    <a class="nav-button my-progress-button" href="/stats">My Progress</a>
 </header>
